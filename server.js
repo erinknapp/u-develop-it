766204e7fb5db11778501859ae95c12aea297868
Erin Knapp <erin.knapp@gmail.com>
2022-01-26 16:54:10 -0500

    modifies get routes to use JOIN

--- server.js
+++ server.js
@@ -16,15 +16,19 @@ const db = mysql.createConnection(
     // Your MySQL username,
     user: 'root',
     // Your MySQL password
-    password: '',
+    password: 'Pbandjelly27!',
     database: 'election'
   },
   console.log('Connected to the election database.')
 );
 
-// Get all candidates
+// Get all candidates and party affiliations
 app.get('/api/candidates', (req, res) => {
-  const sql = `SELECT * FROM candidates`;
+  const sql = `SELECT candidates. *, parties.name
+                AS party_name
+                FROM candidates
+                LEFT JOIN parties
+                ON candidates.party_id = parties.id`;
 
   db.query(sql, (err, rows) => {
     if (err) {
@@ -40,7 +44,12 @@ app.get('/api/candidates', (req, res) => {
 
 // Get a single candidate
 app.get('/api/candidate/:id', (req, res) => {
-  const sql = `SELECT * FROM candidates WHERE id = ?`;
+  const sql = `SELECT candidates.*, parties.name
+                AS party_name
+                FROM candidates
+                LEFT JOIN parties
+                ON candidates.party_id = parties.id
+                WHERE candidates.id = ?`;
   const params = [req.params.id];
 
   db.query(sql, params, (err, row) => {
